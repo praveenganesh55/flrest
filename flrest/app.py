062c963dfc8061beb3db7e3ca63b8cb5675f5445
praveenganesh55 <rgpraveen99@gmail.com>
2020-03-20 14:38:25 +0530

    class based view

--- flrest/app.py
+++ flrest/app.py
@@ -7,6 +7,7 @@ import datetime
 from functools import wraps
 from newsapi import NewsApiClient
 import os
+from flask.views import MethodView
 
 app = Flask(__name__)
 
@@ -47,7 +48,10 @@ def token_required(f):
 
         try:
             data = jwt.decode(token, app.config['SECRET KEY'])
+            global current_user
             current_user = User.query.filter_by(public_id=data['public_id']).first()
+            global ca
+            ca=current_user.admin
         except:
             return jsonify({'message' : 'Token is invalid!'}), 401
 
@@ -55,97 +59,98 @@ def token_required(f):
 
     return decorated
 
-@app.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
+class Getall(MethodView):
+        @token_required
+        def get(self,ca):
 
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+            if not ca:
+                return jsonify({'message' : 'Cannot perform that function!'})
 
-    users = User.query.all()
+            users = User.query.all()
 
-    output = []
+            output = []
 
-    for user in users:
+            for user in users:
+                user_data = {}
+                user_data['public_id'] = user.public_id
+                user_data['name'] = user.name
+                user_data['password'] = user.password
+                user_data['admin'] = user.admin
+                output.append(user_data)
+
+            return jsonify({'users' : output})
+app.add_url_rule('/user',view_func=Getall.as_view('Auser'))
+
+class Cuser(MethodView):
+    @token_required
+    def post(self,ca):
+        if not ca:
+            return jsonify({'message' : 'Cannot perform that function!'})
+
+        data = request.get_json(force=True)
+        hashed_password = generate_password_hash(data['password'], method='sha256')
+
+        new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({'message' : 'New user created!'})
+app.add_url_rule('/user',view_func=Cuser.as_view('user'))
+
+class Edituser(MethodView):
+    @token_required
+    def get(self, ca, public_id):
+
+        if not ca:
+            return jsonify({'message': 'Cannot perform that function!'})
+
+        user = User.query.filter_by(public_id=public_id).first()
+
+        if not user:
+            return jsonify({'message': 'No user found!'})
+
         user_data = {}
         user_data['public_id'] = user.public_id
         user_data['name'] = user.name
         user_data['password'] = user.password
         user_data['admin'] = user.admin
-        output.append(user_data)
 
-    return jsonify({'users' : output})
+        return jsonify({'user': user_data})
 
-@app.route('/user/<public_id>', methods=['GET'])
-@token_required
-def get_one_user(current_user, public_id):
+    @token_required
+    def put(self, ca, public_id):
+        if not ca:
+            return jsonify({'message': 'Cannot perform that function!'})
 
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+        user = User.query.filter_by(public_id=public_id).first()
 
-    user = User.query.filter_by(public_id=public_id).first()
+        if not user:
+            return jsonify({'message': 'No user found!'})
 
-    if not user:
-        return jsonify({'message' : 'No user found!'})
+        user.admin = True
+        db.session.commit()
 
-    user_data = {}
-    user_data['public_id'] = user.public_id
-    user_data['name'] = user.name
-    user_data['password'] = user.password
-    user_data['admin'] = user.admin
+        return jsonify({'message': 'The user has been promoted!'})
 
-    return jsonify({'user' : user_data})
+    @token_required
+    def delete(self, ca, public_id):
+        if not ca:
+            return jsonify({'message': 'Cannot perform that function!'})
 
-@app.route('/user', methods=['POST'])
-@token_required
-def create_user(current_user):
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+        user = User.query.filter_by(public_id=public_id).first()
 
-    data = request.get_json(force=True)
-    #return {"message": data}
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+        if not user:
+            return jsonify({'message': 'No user found!'})
 
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
-    db.session.add(new_user)
-    db.session.commit()
+        db.session.delete(user)
+        db.session.commit()
 
-    return jsonify({'message' : 'New user created!'})
+        return jsonify({'message': 'The user has been deleted!'})
 
-@app.route('/user/<public_id>', methods=['PUT'])
-@token_required
-def promote_user(current_user,public_id):
-   if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+app.add_url_rule('/user/<public_id>', view_func=Edituser.as_view('Euser'))
 
-   user = User.query.filter_by(public_id=public_id).first()
-
-   if not user:
-        return jsonify({'message' : 'No user found!'})
-
-   user.admin = True
-   db.session.commit()
-
-   return jsonify({'message' : 'The user has been promoted!'})
-
-@app.route('/user/<public_id>', methods=['DELETE'])
-@token_required
-def delete_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message' : 'No user found!'})
-
-    db.session.delete(user)
-    db.session.commit()
-
-    return jsonify({'message' : 'The user has been deleted!'})
-
-@app.route('/login')
-def login():
+class Login(MethodView):
+ def get(self):
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
@@ -162,45 +167,50 @@ def login():
         return jsonify({'token' : token.decode('UTF-8')})
 
     return jsonify({"message":"Could not Verify"})
+app.add_url_rule('/login',view_func=Login.as_view('login'))
 
-@app.route('/collect',methods=['GET'])
-@token_required
-def collect(current_user):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
-    newsapi = NewsApiClient(api_key=os.environ.get("APIKEY"))
-    lis=["sports","business","technology","entertainment"]
-    for k in lis:
-     for i in range(1,4):
-            top_headlines = newsapi.get_top_headlines(category=k,country='in', page=i)
-            articles=top_headlines['articles']
-            for i in range(len(articles)):
-                 myarticle=articles[i]
-                 new_article = News(author=myarticle['author'],title=myarticle['title'],description=myarticle['description'],url=myarticle['url'],content=myarticle['content'],category=k,date=datetime.date.today())
-                 db.session.add(new_article)
-    db.session.commit()
+class Collect(MethodView):
+        @token_required
+        def get(self,ca):
+            if not ca:
+                return jsonify({'message': 'Cannot perform that function!'})
+            newsapi = NewsApiClient(api_key=os.environ.get("APIKEY"))
+            lis=["sports","business","technology","entertainment"]
+            for k in lis:
+             for i in range(1,4):
+                    top_headlines = newsapi.get_top_headlines(category=k,country='in', page=i)
+                    articles=top_headlines['articles']
+                    for i in range(len(articles)):
+                         myarticle=articles[i]
+                         new_article = News(author=myarticle['author'],title=myarticle['title'],description=myarticle['description'],url=myarticle['url'],content=myarticle['content'],category=k,date=datetime.date.today())
+                         db.session.add(new_article)
+            db.session.commit()
 
 
 
-    return jsonify({"message": "Successfully stored in Database"})
+            return jsonify({"message": "Successfully stored in Database"})
+app.add_url_rule('/collect',view_func=Collect.as_view('collect'))
 
-@app.route('/category/<int:num>',methods=['GET'])
-def category(num):
-    cate=request.args['cate']
-    newss=News.query.filter_by(category=cate,date=datetime.date.today())
-    output=[]
-    for news in newss:
-        news_data={}
-        news_data['author'] = news.author
-        news_data['title'] = news.title
-        news_data['description'] = news.description
-        news_data['url'] = news.url
-        news_data['content'] = news.content
-        output.append(news_data)
-    a = (num * 10) - 9
-    b = (num * 10) + 1
-    return jsonify({"message": output[a:b]})
-
+class Category(MethodView):
+        @token_required
+        def get(self,page):
+            page=request.args['page']
+            cate=request.args['cate']
+            newss=News.query.filter_by(category=cate,date=datetime.date.today())
+            output=[]
+            for news in newss:
+                news_data={}
+                news_data['author'] = news.author
+                news_data['title'] = news.title
+                news_data['description'] = news.description
+                news_data['url'] = news.url
+                news_data['content'] = news.content
+                output.append(news_data)
+            page=int(page)
+            a = (page * 10) - 9
+            b = (page * 10) + 1
+            return jsonify({"message": output[a:b]})
+app.add_url_rule('/category',view_func=Category.as_view('category'))
 
    
 if __name__=='__main__':
